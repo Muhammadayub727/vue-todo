e45e26dcf73ff90bf64c07ab77736fe415611db9
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-07-26 14:49:10 +0500

    Project default

--- src/components/app-info/Appinfo.vue
+++ src/components/app-info/Appinfo.vue
@@ -1,26 +1,32 @@
 <template>
-    <div class="app-info">
-        <p class="fs-3 text-uppercase">Number of all Movies:{{ allMoviesCount }}</p>
-        <p class="fs-4 text-uppercase">Number of movies viewed:0</p>
-    </div>
-    
+  <div class="app-info">
+    <p class="fs-3 text-uppercase">Number of all Movies:{{ allMoviesCount }}</p>
+    <p class="fs-4 text-uppercase">
+      Number of movies viewed:{{ favouriteMoviesCount }}
+    </p>
+  </div>
 </template>
 <script>
-    export default {
-        props: {
-            allMoviesCount: {
-                type:Number,
-                requied: true
-            }
-        }
-    }
+export default {
+  props: {
+    allMoviesCount: {
+      type: Number,
+      requied: true,
+    },
+    favouriteMoviesCount: {
+      type: Number,
+      requied: true,
+    },
+  },
+
+};
 </script>
 
 <style scoped>
-    .app-info {
-        padding: 1.5rem;
-        border-radius: 4px;
-        background: #fcfaf5;
-        box-shadow: 15px 15px 15px rgba(0,0,0,0.15);
+.app-info {
+  padding: 1.5rem;
+  border-radius: 4px;
+  background: #fcfaf5;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
 }
 </style>
--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -1,66 +1,77 @@
 <template>
   <div class="app font-monospace">
     <div class="content">
-      <Appinfo :allMoviesCount="movies.length"/>
-        <div class="search-panel">
-          <SearchPanel/>
-          <AppFilterVue/>
-        </div>
-        <MovieList :movies="movies"/>
-        <MovieAddForm/>
+      <Appinfo
+        :allMoviesCount="movies.length"
+        :favouriteMoviesCount="favouriteCount"
+      />
+      <div class="search-panel">
+        <SearchPanel />
+        <AppFilterVue />
+      </div>
+      <MovieList :movies="movies" />
+      <MovieAddForm />
     </div>
   </div>
 </template>
 
 <script>
-  import Appinfo from '../app-info/Appinfo.vue'
-  import SearchPanel from '../search-panel/SearchPanel.vue';
-  import AppFilterVue from '../app-filter/AppFilter.vue';
-  import MovieList from '../movie-list/MovieList.vue';
-  import MovieAddForm from '../movie-add-form/MovieAddForm.vue';
-  export default {
-    components: {
+import Appinfo from "../app-info/Appinfo.vue";
+import SearchPanel from "../search-panel/SearchPanel.vue";
+import AppFilterVue from "../app-filter/AppFilter.vue";
+import MovieList from "../movie-list/MovieList.vue";
+import MovieAddForm from "../movie-add-form/MovieAddForm.vue";
+export default {
+  components: {
     Appinfo,
     SearchPanel,
     AppFilterVue,
     MovieList,
-    MovieAddForm
+    MovieAddForm,
   },
-    data() {
-      return {
-        movies: [
-          {
-            id: 1,
-            name: 'The Godfather',
-            seen: 701,
-            favoutite: false,
-            like: false
-          },
-          {
-            id: 2,
-            name: 'The Shawshank Redemption',
-            seen: 712,
-            favoutite: true,
-            like: true
-          },
-          {
-            id: 3,
-            name: 'The Godfather: Part II',
-            seen: 702,
-            favoutite: false,
-            like: false
-          },
-          {
-            id: 4,
-            name: 'Pulp Fiction',
-            seen: 771,
-            favoutite: true,
-            like: true
-          },
-        ]
-      }
+  data() {
+    return {
+      movies: [
+        {
+          id: 1,
+          name: "The Godfather",
+          seen: 701,
+          favoutite: false,
+          like: false,
+        },
+        {
+          id: 2,
+          name: "The Shawshank Redemption",
+          seen: 712,
+          favoutite: true,
+          like: true,
+        },
+        {
+          id: 3,
+          name: "The Godfather: Part II",
+          seen: 702,
+          favoutite: false,
+          like: false,
+        },
+        {
+          id: 4,
+          name: "Pulp Fiction",
+          seen: 771,
+          favoutite: true,
+          like: true,
+        },
+      ],
+    };
+  },
+  computed:{
+    favouriteCount(){
+        return this.movies.filter((c) => c.favoutite).length
     },
-  }
+    mounted(){
+        console.log('favouriteCount')
+    }
+}
+};
 </script>
 
 <style scoped>
@@ -74,7 +85,7 @@
   background-color: #fff;
   border-radius: 7px;
   padding: 5rem 0;
-  margin:0 auto;
+  margin: 0 auto;
   /* box-shadow: 0 0 10px 10px #ccc; */
 }
 .search-panel {
@@ -82,6 +93,6 @@
   padding: 1.5rem;
   background: #fcfaf5;
   border-radius: 4px;
-  box-shadow: 15px 15px 15px 15px rgba(0,0,0,0.15);
+  box-shadow: 15px 15px 15px 15px rgba(0, 0, 0, 0.15);
 }
 </style>
