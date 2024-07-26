310f1f5018bbb15436b7956ac6cd3ad6ea4e37a3
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-07-26 06:22:50 +0500

    Project update

--- src/components/movie-list-item/MovieListItem.vue
+++ src/components/movie-list-item/MovieListItem.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <li class="li-group-item d-flex justify-content-between">
     <span class="list-group-item-label">Mahmudov</span>
     <input type="number" class="list-group-item-input" defaultValue="711" />
@@ -50,4 +50,80 @@ export default {};
     background: red;
 }
 
+</style> -->
+<template>
+  <li class="li-group-item d-flex justify-content-between">
+    <span class="list-group-item-label">Mahmudov</span>
+    <input type="number" class="list-group-item-input" defaultValue="711" />
+    <div class="d-flex justify-content-center align-items-center">
+      <button type="button" class="btn-cookie btn-sm">
+        <i class="fa fa-heart"></i>
+      </button>
+      <button type="button" class="btn-cookie btn-sm">
+        <i class="fas fa-trash"></i>
+      </button>
+      <i class="fas fa-star"></i>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.li-group-item {
+    padding: 15px 20px;
+    border-bottom: 1px solid #3d5a80;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.list-group-item:last-child {
+    border-bottom: none;
+}
+.list-group-item-label {
+    line-height: 35px;
+    font-size: 20px;
+    cursor: pointer;
+    width: 550px;
+    color: #333;
+}
+.list-group-item-input {
+    line-height: 35px;
+    font-size: 20px;
+    text-align: center;
+    border: 2px solid #3d5a80;
+    border-radius: 5px;
+    padding: 5px 10px;
+    width: 100px;
+    margin: 0 10px;
+    background-color: #fff;
+    color: #333;
+}
+.list-group-item-input:focus {
+    border-color: #2a4d69;
+    outline: none;
+}
+.btn-cookie {
+    width: 35px;
+    height: 35px;
+    margin: 3px;
+    font-size: 17px;
+    border: none;
+    cursor: pointer;
+    background: #f8f9fa;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: background 0.3s, transform 0.2s;
+}
+.btn-cookie:hover {
+    background: #e2e6ea;
+    transform: scale(1.1);
+}
+.btn-cookie:active {
+    background: #d6d6d6;
+}
 </style>
