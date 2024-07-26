d11659361037705ac154e744b353316da5488269
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-07-26 09:58:25 +0500

    Todo update

--- src/components/movie-list-item/MovieListItem.vue
+++ src/components/movie-list-item/MovieListItem.vue
@@ -1,66 +1,13 @@
-<!-- <template>
-  <li class="li-group-item d-flex justify-content-between">
-    <span class="list-group-item-label">Mahmudov</span>
-    <input type="number" class="list-group-item-input" defaultValue="711" />
-    <div class="d-flex justify-content-center align-items-center">
-      <button type="button" class="btn-cookie btn-sm">
-        <i class="fa fa-heart"></i>
-      </button>
-      <button type="button" class="btn-cookie btn-sm">
-        <i class="fas fa-trash"></i>
-      </button>
-      <i class="fas fa-star"></i>
-    </div>
-  </li>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style scoped>
-.li-group-item {
-    padding: 15px 20px;
-    border-bottom: 1px solid #3d5a80;
-
-}
-.list-group-item:last-child{
-    border-bottom: none;
-}
-.list-group-item span{
-    line-height: 35px;
-    font-size: 20px;
-    cursor: pointer;
-    width: 550px;
-}
-.list-group-item input{
-    line-height: 35px;
-    font-size: 20px;
-    text-align: center;
-    border: 0;
-    outline: none;
-}
-.list-group-item button{
-    width: 35px;
-    height: 35px;
-    margin: 3px;
-    font-size: 17px;
-    border: none;
-    cursor: pointer;
-    background: red;
-}
-
-</style> -->
 <template>
   <li class="li-group-item d-flex justify-content-between">
-    <span class="list-group-item-label">Mahmudov</span>
+    <span class="list-group-item-label"><i class="fas fa-user user"></i>Mahmudov</span>
     <input type="number" class="list-group-item-input" defaultValue="711" />
     <div class="d-flex justify-content-center align-items-center">
       <button type="button" class="btn-cookie btn-sm">
-        <i class="fa fa-heart"></i>
+        <i class="fa fa-heart heart"></i>
       </button>
       <button type="button" class="btn-cookie btn-sm">
-        <i class="fas fa-trash"></i>
+        <i class="fa fa-trash" aria-hidden="true"></i>
       </button>
       <i class="fas fa-star"></i>
     </div>
@@ -123,7 +70,11 @@ export default {};
     background: #e2e6ea;
     transform: scale(1.1);
 }
-.btn-cookie:active {
+.btn-cookie:active {  
     background: #d6d6d6;
 }
+  .user{
+    color: rgb(20, 20, 71);
+    margin-right: 10px;
+  }
 </style>
