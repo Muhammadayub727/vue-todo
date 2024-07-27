96ffd2496255eee568c86a5560e2d7ccb9ac3e45
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-07-27 15:21:44 +0500

    Project update

--- src/components/app-filter/AppFilter.vue
+++ src/components/app-filter/AppFilter.vue
@@ -1,17 +1,57 @@
-<template>
+    <template>
     <div class="btn-group">
-        <button class="btn btn-success" type="button">All movies</button>
-        <button class="btn btn-outline-success" type="button">Popular movies</button>
-        <button class="btn btn-outline-success" type="button">Most viewed movies</button>
+        <button
+        v-for="btn in filterButtons"
+        :key="btn.name"
+        class="btn"
+        type="button"
+        :class="[filterName === btn.name ? 'btn-success' : 'btn-outline-success']"
+        @click="filterHandler(btn.name)"
+        >
+        {{ btn.title }}
+        </button>
     </div>
-</template>
+    </template>
 
-<script>
-export default {
+    <script>
+    export default {
+    props: {
+        updateFilterHandler: {
+        type: Function,
+        required: true,
+        },
+        filterName: {
+        type: String,
+        required: true,
+        },
+    },
+    data() {
+        return {
+        filterButtons: [
+            {
+            title: "All movies",
+            name: "all",
+            },
+            {
+            title: "Popular movies",
+            name: "popular",
+            },
+            {
+            title: "Most viewed movies",
+            name: "mostViewed",
+            },
+        ],
+        filter: "all",
+        };
+    },
+    methods: {
+        filterHandler(filter) {
+        this.filter = filter;
+        this.updateFilterHandler(this.filter);
+        },
+    },
+    };
+    </script>
 
-}
-</script>
-
-<style>
-
-</style>
+    <style>
+    </style>
--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -1,141 +1,3 @@
-<!-- <template>
-  <div class="app font-monospace">
-    <div class="content">
-      <Appinfo
-        :allMoviesCount="movies.length"
-        :favouriteMoviesCount="favouriteCount"
-      />
-      <div class="search-panel">
-        <SearchPanel :updateTermHandler="updateTermHandler"/>
-        <AppFilterVue />
-      </div>
-      <MovieList :movies="onSearchHandler(movies,term)" @onLike="onLikeHandler" @onFavourite="onFavouriteHandler" @onRemove="onRemoveHandler"/>
-      <MovieAddForm  @createMovie="createMovie"/>
-    </div>
-  </div>
-</template>
-
-<script>
-import Appinfo from "../app-info/Appinfo.vue";
-import SearchPanel from "../search-panel/SearchPanel.vue";
-import AppFilterVue from "../app-filter/AppFilter.vue";
-import MovieList from "../movie-list/MovieList.vue";
-import MovieAddForm from "../movie-add-form/MovieAddForm.vue";
-
-export default {
-  components: {
-    Appinfo,
-    SearchPanel,
-    AppFilterVue,
-    MovieList,
-    MovieAddForm,
-  },
-  data() {
-    return {
-      movies: [
-        {
-          id: 1,
-          name: " Godfather",
-          seen: 701,
-          favourite: false,
-          like: false,
-        },
-        {
-          id: 2,
-          name: " Shawshank Redemption",
-          seen: 712,
-          favourite: false,
-          like: false,
-        },
-        {
-          id: 3,
-          name: " Godfather: Part II",
-          seen: 702,
-          favourite: false,
-          like: false,
-        },
-        {
-          id: 4,
-          name: "Pulp Fiction",
-          seen: 771,
-          favourite: false,
-          like: false,
-        },
-      ],
-      term:'',
-    };
-  },
-  methods: {
-    createMovie(item) {
-      this.movies.push(item);
-    },
-    onLikeHandler(id) {
-      console.log(id);
-      this.movies = this.movies.map((item) => {
-        if (item.id == id) {
-          item.like = !item.like;
-        }
-        return item;
-      });
-    },
-    onFavouriteHandler(id) {
-      console.log(id);
-      this.movies = this.movies.map((item) => {
-        if (item.id == id) {
-          item.favourite = !item.favourite;
-        }
-        return item;
-      });
-    },
-    onRemoveHandler(id) {
-      console.log(id);
-      this.movies = this.movies.filter(c => c.id != id);
-    },
-
-    onSearchHandler(arr,term) {
-      if (term.length === 0) {
-        return arr
-      }
-      return arr.filter((c) => c.name.toLowerCase().indexOf(term) > -1)
-    },
-
-    updateTermHandler(term){
-      this.term = term
-    }
-  },
-  computed: {
-    favouriteCount() {
-      return this.movies.filter((c) => c.favourite).length;
-    },
-  },
-  mounted() {
-    console.log("favouriteCount");
-  },
-};
-</script>
-
-<style scoped>
-.app {
-  height: 100vh;
-  color: #000;
-}
-.content {
-  width: 1000px;
-  min-height: 720px;
-  background-color: #fff;
-  border-radius: 7px;
-  padding: 5rem 0;
-  margin: 0 auto;
-  /* box-shadow: 0 0 10px 10px #ccc; */
-}
-.search-panel {
-  margin-top: 2rem;
-  padding: 1.5rem;
-  background: #fcfaf5;
-  border-radius: 4px;
-  box-shadow: 15px 15px 15px 15px rgba(0, 0, 0, 0.15);
-}
-</style> -->
 <template>
   <div class="app font-monospace">
     <div class="content">
@@ -145,11 +7,11 @@ export default {
       />
       <div class="search-panel">
         <SearchPanel :updateTermHandler="updateTermHandler" />
-        <AppFilterVue />
+        <AppFilterVue :updateFilterHandler="updateFilterHandler" :filterName="filter"/>
       </div>
       <template v-if="filteredMovies.length">
         <MovieList
-          :movies="filteredMovies"
+          :movies="onFilterHandler(filteredMovies, filter)"
           @onLike="onLikeHandler"
           @onFavourite="onFavouriteHandler"
           @onRemove="onRemoveHandler"
@@ -157,8 +19,10 @@ export default {
       </template>
       <template v-else>
         <div class="no-data">
-          <!-- <p>No data found</p> -->
-          <img src="https://i.pinimg.com/originals/0e/c0/db/0ec0dbf1e9a008acb9955d3246970e15.gif" alt="No data" />
+          <img
+            src="https://i.pinimg.com/originals/0e/c0/db/0ec0dbf1e9a008acb9955d3246970e15.gif"
+            alt="No data"
+          />
         </div>
       </template>
       <MovieAddForm @createMovie="createMovie" />
@@ -184,12 +48,37 @@ export default {
   data() {
     return {
       movies: [
-        { id: 1, name: "Godfather", seen: 701, favourite: false, like: false },
-        { id: 2, name: "Shawshank Redemption", seen: 712, favourite: false, like: false },
-        { id: 3, name: "Godfather: Part II", seen: 702, favourite: false, like: false },
-        { id: 4, name: "Pulp Fiction", seen: 771, favourite: false, like: false },
+        { 
+          id: 1,
+          name: "Godfather", 
+          seen: 701, 
+          favourite: false, 
+          like: false 
+        },
+        {
+          id: 2,
+          name: "Shawshank Redemption",
+          seen: 222,
+          favourite: false,
+          like: false,
+        },
+        {
+          id: 3,
+          name: "Godfather: Part II",
+          seen: 442,
+          favourite: false,
+          like: false,
+        },
+        {
+          id: 4,
+          name: "Pulp Fiction",
+          seen: 771,
+          favourite: false,
+          like: false,
+        },
       ],
-      term: '',
+      term: "",
+      filter: 'all',
     };
   },
   methods: {
@@ -213,21 +102,37 @@ export default {
       });
     },
     onRemoveHandler(id) {
-      this.movies = this.movies.filter(c => c.id !== id);
+      this.movies = this.movies.filter((c) => c.id !== id);
     },
     onSearchHandler(arr, term) {
       if (term.length === 0) {
         return arr;
       }
-      return arr.filter(c => c.name.toLowerCase().indexOf(term) > -1);
+      return arr.filter((c) => c.name.toLowerCase().indexOf(term) > -1);
+    },
+    onFilterHandler(arr,filter) {
+      switch (filter) {
+        case 'popular':
+          return arr.filter((c) => c.like);
+        case 'mostViewed':
+          return arr.filter((c) => c.viewers > 700);
+        case 'favourite':
+          return arr.filter((c) => c.favourite);
+        default:
+          return arr
+      }
     },
     updateTermHandler(term) {
       this.term = term;
     },
+
+    updateFilterHandler(filter) {
+      this.filter = filter
+    }
   },
   computed: {
     favouriteCount() {
-      return this.movies.filter(c => c.favourite).length;
+      return this.movies.filter((c) => c.favourite).length;
     },
     filteredMovies() {
       return this.onSearchHandler(this.movies, this.term);
@@ -264,7 +169,7 @@ export default {
 
 .no-data img {
   width: 1000px;
-  height:250px;
+  height: 250px;
   border-radius: 10px;
   box-shadow: 15px 15px 15px 15px rgba(0, 0, 0, 0.15);
 }
