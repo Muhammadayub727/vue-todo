45e616eb0a6b8b2945f5128c969fe9045a859eea
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-08-01 17:38:38 +0500

    server update

--- src/components/movie-add-form/MovieAddForm.vue
+++ src/components/movie-add-form/MovieAddForm.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div class="movie-add-form">
       <h1>Add Movie</h1>
       <form class="add-form d-flex" @submit.prevent="addMovie">
@@ -53,4 +53,54 @@
     box-shadow: 15px 15px 15px 15px rgba(0, 0, 0, 0.15);
   }
   </style>
+  -->
+
+  <template>
+    <div class="movie-add-form">
+      <h1>Add Movie</h1>
+      <form class="add-form d-flex" @submit.prevent="addMovie">
+        <input
+          type="text"
+          class="form-control new-movie-label"
+          placeholder="Movie"
+          v-model="name"
+        />
+        <input
+          type="number"
+          class="form-control new-movie-label"
+          placeholder="Seen how many times?"
+          v-model.number="viewers"
+        />
+        <PrimaryButton class="btn btn-outline-success" type="submit">Add</PrimaryButton>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import { v4 as uuidv4 } from 'uuid'; 
+  
+  export default {
+    data() {
+      return {
+        name: '',
+        viewers: 0
+      };
+    },
+    methods: {
+      addMovie() {
+        if (!this.name || !this.viewers) return;
+        const newMovie = {
+          id: uuidv4(), // Generate unique ID
+          name: this.name,
+          viewers: this.viewers,
+          favourite: false,
+          like: false,
+        };
+        this.$emit('createMovie', newMovie);
+        this.name = '';
+        this.viewers = 0;
+      }
+    },
+  };
+  </script>
   
